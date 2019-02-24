6c73a48e7227a259c680f38d1e0084ab584862b8
marlou <pro@marc-loupias.fr>
2019-02-24 09:35:17 +0100

    fix(marked-renderer-impl): fix link function (#16)
    the .md to .html replace must occurs only when links ends with .md
    
    fix #15

--- bin/marked-renderer-html-impl.js
+++ bin/marked-renderer-html-impl.js
@@ -1,8 +1,11 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 function link(href, title, text) {
-    const htmlHref = href.substring(0, href.length - 2) + 'html';
-    return `<a href="${htmlHref}">${text}</a>`;
+    if (href.substring(href.length - 3, href.length) === '.md') {
+        const htmlHref = href.substring(0, href.length - 2) + 'html';
+        return `<a href="${htmlHref}">${text}</a>`;
+    }
+    return `<a href="${href}">${text}</a>`;
 }
 exports.default = {
     link
